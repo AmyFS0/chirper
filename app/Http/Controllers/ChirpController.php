8ed554e7a40463b8c1eb195788ed055989a42598
AmyFS0 <amyfajardo427@gmail.com>
2026-08-29 21:40:57 -0600

    commit 5

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -11,26 +11,7 @@ class ChirpController extends Controller
 {
     public function index(): View
     {
-        $chirps = [
-            [
-                'id' => 1,
-                'author' => 'Taylor Otwell',
-                'message' => 'The only way to do great work is to love what you do.',
-                'time' => '2 hours ago',
-            ],
-            [
-                'id' => 2,
-                'author' => 'Jeffrey Way',
-                'message' => 'Clean code is better than clever code.',
-                'time' => '5 hours ago',
-            ],
-            [
-                'id' => 3,
-                'author' => 'Josh Cirre',
-                'message' => 'Learning Laravel has been a game changer for me!',
-                'time' => '1 day ago',
-            ],
-        ];
+        $chirps = Chirp::with('user')->latest()->take(50)->get();
 
         return view('home', [
             'chirps' => $chirps,
---
+++ app/Models/Chirp.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Chirp extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'message',
+        'user_id',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
